a20c658a688b3cd594b10d810862f974bb34fdab
pma-itfs <admin@pma-itfs.net>
2026-03-11 01:00:12 +0000

    agrega pipeline github actions

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.get("/")
 def home():
-    return "Deploy automático OK: GitHub Actions -> ECR -> ECS Fargate"
+    return "Deploy automático OK: GitHub Actions -> ECR -> ECS Fargate Marvin "
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
